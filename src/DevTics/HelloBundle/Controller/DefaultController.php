1d140ede3abff972af90071b8c9c2cb57b6e3a43
José Francisco Díaz <ubicalo@ubicalo12>
2014-07-18 10:49:31 -0500

    primeras pruebas de twig

--- src/DevTics/HelloBundle/Controller/DefaultController.php
+++ src/DevTics/HelloBundle/Controller/DefaultController.php
@@ -8,7 +8,63 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('DevTicsHelloBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('DevTicsHelloBundle:Default:index.html.twig', [
+            'name' => $name,
+            'articles' => [ 
+                [
+                    'title' => 'titulo del articulo',
+                    'authorName' => 'autor',
+                    'body' =>  'articulo '
+                ],
+                [
+                    'title' => 'titulo del articulo1',
+                    'authorName' => 'autor',
+                    'body' =>  'articulo '
+                ],
+                [
+                    'title' => 'titulo del articulo2',
+                    'authorName' => 'autor',
+                    'body' =>  'articulo '
+                ],
+                [
+                    'title' => 'titulo del articulo3',
+                    'authorName' => 'autor',
+                    'body' =>  'articulo '
+                ],
+            ]
+        ]);
+    }
+    
+    public function recentArticlesAction ($max) {
+        $i = 0;
+        return $this->render('DevTicsHelloBundle:Pruebas:recentList.html.twig', [
+            'articles' => [ 
+                [
+                    'slug' => $i++,
+                    'title' => 'titulo del articulo db',
+                    'authorName' => 'autor',
+                    'body' =>  'articulo '
+                ],
+                [
+                    'slug' => $i++,
+                    'title' => 'titulo del articulo1 db',
+                    'authorName' => 'autor',
+                    'body' =>  'articulo '
+                ],
+                [
+                    'slug' => $i++,
+                    'title' => 'titulo del articulo2 db',
+                    'authorName' => 'autor',
+                    'body' =>  'articulo '
+                ],
+                [
+                    'slug' => $i++,
+                    'title' => 'titulo del articulo3 db',
+                    'authorName' => 'autor',
+                    'body' =>  'articulo '
+                ],
+            ]
+        ]);
     }
     public function ttAction(){
 //        $r =  new \Symfony\Component\HttpFoundation\Response();
