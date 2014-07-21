3a88244589db06bd8da6aaec49fd2cf8aa22bfbe
José Francisco Díaz <ubicalo@ubicalo12>
2014-07-21 17:59:16 -0500

    doctrine init

--- src/DevTics/HelloBundle/Controller/DefaultController.php
+++ src/DevTics/HelloBundle/Controller/DefaultController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     public function indexAction($name)
-    {
+    { //coment
         return $this->render('DevTicsHelloBundle:Default:index.html.twig', [
             'name' => $name,
             'articles' => [ 
